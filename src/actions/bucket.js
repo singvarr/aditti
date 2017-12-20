63cbdcb0ecc7c7500c6c18fdf8556f5c4d8d855f
sihorjs <sihorjs@gmail.com>
2017-12-20 10:46:31 +0200

    add actions for redux

---
+++ src/actions/bucket.js
@@ -0,0 +1,40 @@
+export const ADD_ITEM = "ADD_ITEM";
+export const REMOVE_ITEM = "REMOVE_ITEM";
+export const INCREASE_QUANTITY = "INCREASE_QUANTITY";
+export const DECREASE_QUANTITY = "DECREASE_QUANTITY";
+export const CLEAR_BUCKET = "CLEAR_BUCKET";
+
+export function addItem(name, quantity) {
+	return {
+		type: ADD_ITEM,
+		name,
+		quantity
+	}
+}
+
+export function removeItem(name) {
+	return {
+		type: REMOVE_ITEM,
+		name
+	}
+}
+
+export function increaseQuantity(name) {
+	return {
+		type: INCREASE_QUANTITY,
+		name
+	}
+}
+
+export function decreaseQuantity(name) {
+	return {
+		type: DECREASE_QUANTITY,
+		name
+	}
+}
+
+export function clearBucket() {
+	return {
+		type: CLEAR_BUCKET
+	}
+}
---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+export * from './bucket.js';
+export * from './filter.js';
