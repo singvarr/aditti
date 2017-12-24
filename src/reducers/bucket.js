e7c09798bdb0eefa27337881a3800c5ff404f507
sihorjs <sihorjs@gmail.com>
2017-12-24 21:33:45 +0200

    refactor reducer

--- src/reducers/bucket.js
+++ src/reducers/bucket.js
@@ -1,28 +1,30 @@
 import { 
-	ADD_ITEM, 
 	REMOVE_ITEM, 
 	INCREASE_QUANTITY, 
 	DECREASE_QUANTITY,
 	CLEAR_BUCKET,
-	CALCULATE_DISCOUNT
 } from "../actions";
 
 export default function reducer(state = [], action) {
+	const index = state.findIndex(item => item.name == action.name);
+
 	switch(action.type) {
-		case ADD_ITEM: return
-			
-		case REMOVE_ITEM: return;
+		case REMOVE_ITEM: 
+			state[index].quantity = 0;
+			break;
 		
 		case INCREASE_QUANTITY:
-			return state.map(item => {
-				if (item.name == action.name) {
-					return Object.assign({}, item, { quantity: item.quantity + 1 })
-				}
-				return item
-			})
+			state[index].quantity += 1;
+			break;
 
-		case DECREASE_QUANTITY: return;
-		case CLEAR_BUCKET: return; 
+		case DECREASE_QUANTITY: 
+			state[index].quantity -= 1;
+			break;
+		
+		case CLEAR_BUCKET: return state.map(item => Object.assign({}, item, {quantity: 0}))
+
 		default: return state;
 	}
+
+	return [...state.slice(0, index), state[index], ...state.slice(index + 1)]
 }
