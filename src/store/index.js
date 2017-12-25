7b8f0fb1169af33fd2a51c80101fab8d5a401998
sihorjs <sihorjs@gmail.com>
2017-12-25 15:55:42 +0200

    delete listening

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,5 @@ import reducer from "../reducers";
 import state from "./state.json";
 
 const store = createStore(reducer, state);
-store.subscribe(() => console.log(store.getState()));
 
 export default store;
