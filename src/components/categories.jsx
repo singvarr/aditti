02c7759c7de8ebe6ebeadb8cf06f1d1fe50f5443
sihorjs <sihorjs@gmail.com>
2017-12-26 15:16:34 +0200

    add custom controls for carousel

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React, {Component} from "react";
 import Carousel from "nuka-carousel";
 
-function Categories(props) {
-    return (
-        <Carousel >
-        {props.categories.map((category, index) => {
-            return <div className="category" key={index}>
-                <div className="category-img">
-                    <img src={category.src} alt={category.name}/>
-                </div>
-                <div className="category-title">
-                    <span>{category.name}</span>
-                    <button name="shop"
-                        onClick={() => props.onFilter(category.name)}
-                    >Buy</button>
-                </div>
-            </div>
-        })}
-        </Carousel>
-    )
+const decoratorStyle = {
+    width: 30,
+    height: 90,
+    outline: 'none',
+    border: 'none',
+    background: "url('./assets/img/control-left.png') 50% 50%"
+}
+
+class Categories extends Component {
+    render() {
+        return (
+            <Carousel 
+                width={1024}
+                slidesToShow={3}
+                ref="carousel"
+                dragging={false}
+                style={{margin: '0 auto'}}
+                decorators={[
+                    {
+                        component: () => <button
+                            style={decoratorStyle} 
+                            onClick={() => this.refs.carousel.previousSlide()}/>,
+                        position: 'CenterLeft',
+                        style: {left: -40}
+                    },
+                    {
+                        component: () => <button 
+                            style={Object.assign({}, decoratorStyle, {transform: 'rotate(180deg)'})}
+                            onClick={() => this.refs.carousel.nextSlide()}/>,
+                        position: 'CenterRight',
+                        style: {right: -40}
+                    },
+                ]}
+            >
+                {this.props.categories.map((category, index) => {
+                    return <div className="category" key={index}>
+                        <div className="category-img">
+                            <img src={category.src} alt={category.name}/>
+                        </div>
+                        <div className="category-title">
+                            <span>{category.name}</span>
+                            <button name="shop"
+                                onClick={() => this.props.onFilter(category.name)}
+                            >Buy</button>
+                        </div>
+                    </div>
+                })}
+            </Carousel>
+        )
+    }
 }
 
 export default Categories;
