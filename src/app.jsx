ee8c07c99955190536b504369f28a643034a40fa
sihorjs <sihorjs@gmail.com>
2018-11-08 17:19:12 +0200

    use resolve aliases in main js files, prettify files

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
-import Header from './components/header.jsx'
-import CarouselWrapper from './components/carousel.jsx';
-import CategoriesContainer from './containers/CategoriesContainer';
-import GalleryContainer from "./containers/GalleryContainer";
-import Footer from "./components/footer.jsx";
+import Header from "components/header";
+import CarouselWrapper from "components/carousel";
+import CategoriesContainer from "containers/CategoriesContainer";
+import GalleryContainer from "containers/GalleryContainer";
+import Footer from "components/footer";
 
-import links from './store/links.json';
-import slides from './store/slides.json';
+import links from "store/links";
+import slides from "store/slides";
 
 function App() {
-	return [
-		<Header key='header' links={links.mainMenu}/>,
-		<main key='main'>
-			<CarouselWrapper slides={slides}/>
-			<CategoriesContainer/>
-			<GalleryContainer/>
-		</main>,
-		<Footer key='footer' linksList={links.footerLinks}/>
-	]
+    return [
+        <Header key="header" links={links.mainMenu} />,
+        <main key="main">
+            <CarouselWrapper slides={slides} />
+            <CategoriesContainer />
+            <GalleryContainer />
+        </main>,
+        <Footer key="footer" linksList={links.footerLinks} />
+    ];
 }
 
 export default App;
