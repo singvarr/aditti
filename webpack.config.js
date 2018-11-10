2a20685b9b49b65bc8eee1f8b24bcd4c7dc2c73e
sihorjs <sihorjs@gmail.com>
2018-11-10 19:05:18 +0200

    add source map for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const webpack = require("webpack");
 const proxyUrl = "http://localhost:3000/";
 
 module.exports = {
+    devtool: "cheap-module-eval-source-map",
     entry: path.resolve(__dirname, "src", "index.js"),
     output: {
         path: path.resolve(__dirname, "public"),
