7c9fbb669ffba4ab34eba9965edfec0d8e5c7da6
sihorjs <sihorjs@gmail.com>
2018-11-10 17:25:25 +0200

    delete extension in import path

--- src/containers/BucketContainer.js
+++ src/containers/BucketContainer.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 
-import Bucket from "components/bucket.jsx";
+import Bucket from "components/bucket";
 import {
     increaseQuantity,
     decreaseQuantity,
