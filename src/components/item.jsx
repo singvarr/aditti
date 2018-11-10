5c1480bc52625e3799aae1f853429545179d70b8
sihorjs <sihorjs@gmail.com>
2018-11-11 00:14:19 +0200

    add propTypes, format code, use connect function in Item component

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { increaseItemQuantity } from "actions/cart";
 
 function Item(props) {
-	return (
-		<div className="item">
-			<figure><img
-				src={props.src}
-				alt={props.name}/>
-			</figure>
-			<figcaption>
-				<span className="title">{props.name}</span>
-				<span className="price">$ {props.price}</span>
-				<button 
-					name="buy"
-					value={props.price}
-					onClick={() => props.onAdd(props.name)}>Buy now
-				</button>
-			</figcaption>
-		</div>
-	)
+    return (
+        <div className="item">
+            <figure>
+                <img src={props.src} alt={props.name} />
+            </figure>
+            <figcaption>
+                <span className="title">{props.name}</span>
+                <span className="price">$ {props.price}</span>
+                <button
+                    name="buy"
+                    value={props.price}
+                    onClick={() => props.onAdd(props.id)}
+                >
+                    Buy now
+                </button>
+            </figcaption>
+        </div>
+    );
 }
 
-export default Item;
+Item.propTypes = {
+    onAdd: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    category: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired
+};
+
+function mapDispatchToProps(dispatch) {
+    return {
+        onAdd: id => dispatch(increaseItemQuantity(id))
+    };
+}
+
+export default connect(null, mapDispatchToProps)(Item);
