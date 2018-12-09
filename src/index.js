5a071f9adc26f8e88b015acd96d606ea483043f3
sihorjs <sihorjs@gmail.com>
2018-12-09 18:43:50 +0200

    move Menu component on top level, use Fragment to wrap BrowserRouter  content

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Header from "components/Header";
+import Menu from "components/Menu";
 import Footer from "components/Footer";
 import CatalogueRoute from "routes/CatalogueRoute";
 import CartRoute from "routes/CartRoute";
@@ -16,8 +17,9 @@ import links from "store/links";
 render(
     <Provider store={store}>
         <BrowserRouter>
-            <div>
-                <Header links={links.mainMenu} />
+            <Fragment>
+                <Header />
+                <Menu menu={links.mainMenu}/>
                 <main>
                     <Switch>
                         <Route exact path="/" component={CatalogueRoute} />
@@ -25,7 +27,7 @@ render(
                     </Switch>
                 </main>
                 <Footer linksList={links.footerLinks} />
-            </div>
+            </Fragment>
         </BrowserRouter>
     </Provider>,
     document.getElementById("root")
