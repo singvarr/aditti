95fa5f25c9ea665f3e92af26dfc6ebef33b020f6
sihorjs <sihorjs@gmail.com>
2018-12-09 15:57:56 +0200

    create separate component for autosuggest

---
+++ src/components/Autosuggest/Autosuggest.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import "./Autosuggest.less";
+
+class Autosuggest extends Component {
+    render() {
+        return (
+            <form className={classnames("autosuggest", this.props.className)}>
+                <input
+                    className="autosuggest__search"
+                    name="autosuggest-search"
+                />
+                <button
+                    type="button"
+                    className="autosuggest__submit flaticon-search btn-reset"
+                />
+            </form>
+        );
+    }
+}
+
+Autosuggest.propTypes = {
+    className: PropTypes.string
+};
+
+Autosuggest.defaultProps = {
+    className: null
+};
+
+export default Autosuggest;
