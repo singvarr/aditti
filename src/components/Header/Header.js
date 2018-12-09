1466d7637b5da340242b332eb5b76fe895118f22
sihorjs <sihorjs@gmail.com>
2018-12-09 18:49:21 +0200

    refactor header styles and markup, delete Menu component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
 
-import Menu from "components/Menu";
-import CartIcon from "components/CartIcon";
+import Autosuggest from "components/Autosuggest";
+import CartTotal from "components/CartTotal";
 
-function Header(props) {
+import "./Header.less";
+
+function Header() {
     return (
-        <header key="header">
-            <div className="wrapper">
-                <Link to="/">
-                    <div className="logo">
-                        <img src="./assets/img/logo.png" alt="logo" />
-                    </div>
-                </Link>
-                <form action="post" id="search-form">
-                    <input type="search" name="search-field" />
-                    <button type="submit" className="flaticon-search" />
-                </form>
-                <CartIcon />
-            </div>
-            <Menu menu={props.links} />
+        <header className="header">
+            <Link to="/" className="logo-link">
+                <img className="logo" src="./assets/img/logo.png" alt="logo" />
+            </Link>
+            <Autosuggest />
+            <CartTotal />
         </header>
     );
 }
-
-Header.propTypes = {
-    links: PropTypes.arrayOf(
-        PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            href: PropTypes.string.isRequired
-        }).isRequired
-    ).isRequired
-};
 
 export default Header;
