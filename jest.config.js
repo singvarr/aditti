2bc0974c4fafa8ff6336c422dde9fccf4caf6041
sihorjs <sihorjs@gmail.com>
2019-01-08 13:41:32 +0200

    fix fetch reference for tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
-const fetch = require("node-fetch");
-
 module.exports = {
-    globals: { fetch },
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
             "<rootDir>/config/__mocks__/fileMock.js",
