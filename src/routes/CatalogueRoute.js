f04db4806592437719b613f204c7942497bea78a
sihorjs <sihorjs@gmail.com>
2019-01-13 13:50:11 +0200

    rename carousel in catalogue route

--- src/routes/CatalogueRoute.js
+++ src/routes/CatalogueRoute.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from "react";
 
-import Carousel from "components/Carousel";
+import FeaturedProducts from "components/FeaturedProducts";
 import Categories from "components/Categories";
 import Products from "components/Products";
 
 function CatalogueRoute() {
     return (
         <Fragment>
-            <Carousel />
+            <FeaturedProducts />
             <Categories />
             <Products />
         </Fragment>
