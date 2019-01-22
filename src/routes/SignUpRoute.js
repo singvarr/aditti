1d26874018b0490bd236761ffc553d8d523bcc96
sihorjs <sihorjs@gmail.com>
2019-01-22 22:28:20 +0200

    add route for registration

---
+++ src/routes/SignUpRoute.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SignUpForm from "components/SignUpForm";
+
+function SignUpRoute() {
+    return <SignUpForm className="signup-section" />;
+}
+
+export default SignUpRoute;
