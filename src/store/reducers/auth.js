76b98e9c23a47ddd53a307e5bd3449260b0bda8a
sihorjs <sihorjs@gmail.com>
2019-01-23 11:36:33 +0200

    add auth reducer

---
+++ src/store/reducers/auth.js
@@ -0,0 +1,10 @@
+const initialState = false;
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+    default:
+        return state;
+    }
+}
+
+export default reducer;
