f5cbb48f2bf41f7dd4d6de72ace6fb744e5320e4
sihorjs <sihorjs@gmail.com>
2019-01-27 12:30:19 +0200

    delete auth reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from "redux";
 
-import auth from "reducers/auth";
 import carousel from "reducers/carousel";
 import cart from "reducers/cart";
 import categories from "reducers/categories";
 import products from "reducers/products";
 
 const reducer = combineReducers({
-    auth,
     carousel,
     cart,
     categories,
