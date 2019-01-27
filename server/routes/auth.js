45a0f170acecae2f08c1f9ef6a3a075c4e8bc856
sihorjs <sihorjs@gmail.com>
2019-01-27 12:28:21 +0200

    handle save of new user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,41 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
+const User = require("../models/User");
+
 router.post("/signup", (req, res) => {
-    const data = req.body;
+    const { username, password } = req.body;
+
+    User.findOne({ username }, (error, user) => {
+        if (error) {
+            res.sendStatus(500);
+        } else if (user) {
+            res.send({
+                error: true,
+                data: "User has already registered with this nickname"
+            });
+        } else {
+            bcrypt.genSalt(10, (error, salt) => {
+                if (error) {
+                    res.sendStatus(500);
+                }
+
+                bcrypt.hash(password, salt, (error, hash) => {
+                    if (error) {
+                        res.sendStatus(500);
+                    }
+
+                    User({
+                        username,
+                        password: hash
+                    })
+                        .save()
+                        .then(() => res.send({ error: false, data: "ok" }));
+                });
+            });
+        }
+    });
 });
 
 module.exports = router;
