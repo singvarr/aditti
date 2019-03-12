2e9f567f624233c90c14f9ca71fdfba9fa4c81dd
sihor <sihorjs@gmail.com>
2019-03-12 15:58:14 +0200

    add definitins for carousel

---
+++ frontend/store/reducers/carousel.ts
@@ -0,0 +1,48 @@
+import {
+    GET_CAROUSEL_LOADING,
+    GET_CAROUSEL_SUCCESS,
+    GET_CAROUSEL_ERROR
+} from "constants/carousel";
+import { CarouselAction } from "actions/carousel";
+
+export type Slide = {
+    description: string;
+    heading: string;
+    imgSrc: string;
+};
+
+export type CarouselState = {
+    hasError: boolean;
+    isLoading: boolean;
+    data: Array<Slide>;
+};
+
+export const initialState: CarouselState = {
+    data: [],
+    hasError: false,
+    isLoading: false
+};
+
+function reducer(state: CarouselState = initialState, action: CarouselAction) {
+    switch (action.type) {
+        case GET_CAROUSEL_LOADING:
+            return Object.assign({}, state, { isLoading: true });
+
+        case GET_CAROUSEL_SUCCESS:
+            return Object.assign({}, state, {
+                data: action.payload,
+                isLoading: false
+            });
+
+        case GET_CAROUSEL_ERROR:
+            return Object.assign({}, state, {
+                hasError: true,
+                isLoading: false
+            });
+
+        default:
+            return state;
+    }
+}
+
+export default reducer;
