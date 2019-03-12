6ceeed9f9222c6d04fdd6ba977878aad2fa192db
sihor <sihorjs@gmail.com>
2019-03-12 16:12:50 +0200

    add definitions for redux-api-middleware

---
+++ frontend/vendor/redux-api-middleware/index.d.ts
@@ -0,0 +1,27 @@
+declare module "redux-api-middleware" {
+    import { Middleware } from "redux";
+
+    export const RSAA: string;
+    export const apiMiddleware: Middleware;
+
+    export type HTTPVerb =
+        | "GET"
+        | "HEAD"
+        | "POST"
+        | "PUT"
+        | "PATCH"
+        | "DELETE"
+        | "OPTIONS";
+
+    export interface RSAAction<R, S, F> {
+        [propName: string]: {
+            endpoint: string;
+            method: HTTPVerb;
+            body?: any;
+            headers?: { [propName: string]: string };
+            credentials?: "omit" | "same-origin" | "include";
+            bailout?: boolean;
+            types: [R, S, F];
+        };
+    }
+}
