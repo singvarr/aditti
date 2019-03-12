c5b270e3d2059c5366c1566df2a7c3efe0681a0e
sihor <sihorjs@gmail.com>
2019-03-12 15:59:44 +0200

    add defintions for carousel actions

--- frontend/store/actions/carousel.ts
+++ frontend/store/actions/carousel.ts
@@ -1,15 +1,19 @@
-import { RSAA } from "redux-api-middleware";
+import { RSAA, RSAAction } from "redux-api-middleware";
 import {
     GET_CAROUSEL_LOADING,
     GET_CAROUSEL_SUCCESS,
     GET_CAROUSEL_ERROR
-} from "../constants/carousel";
+} from "constants/carousel";
+import { Slide } from "reducers/carousel";
 
 export const carouselEndpoint = "/api/carousel";
 export const headers = { "Content-Type": "application/json" };
 
-
-function getCarousel() {
+function getCarousel(): RSAAction<
+    typeof GET_CAROUSEL_LOADING,
+    typeof GET_CAROUSEL_SUCCESS,
+    typeof GET_CAROUSEL_ERROR
+> {
     return {
         [RSAA]: {
             endpoint: carouselEndpoint,
@@ -23,5 +27,21 @@ function getCarousel() {
         }
     };
 }
+
+export type CarouselRequestAction = {
+    readonly type: typeof GET_CAROUSEL_LOADING;
+};
+export type CarouselErrorAction = {
+    readonly type: typeof GET_CAROUSEL_ERROR;
+};
+export type CarouselSuccessAction = {
+    readonly type: typeof GET_CAROUSEL_SUCCESS;
+    readonly payload: Array<Slide>;
+};
+
+export type CarouselAction =
+    | CarouselErrorAction
+    | CarouselRequestAction
+    | CarouselSuccessAction;
 
 export default getCarousel;
