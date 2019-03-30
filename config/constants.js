a4411283b14010518a7a13934c942c585ca0445f
sihor <sihorjs@gmail.com>
2019-03-30 21:09:34 +0200

    add globals

---
+++ config/constants.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+const DEVELOPMENT = "development";
+const PRODUCTION = "production";
+
+const PROJECT_ROOT = path.resolve(__dirname, "..");
+
+module.exports = {
+    DEVELOPMENT,
+    PRODUCTION,
+    PROJECT_ROOT
+};
