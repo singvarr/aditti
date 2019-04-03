682d766b2bf602653ff09351749471c8846175d4
sihorjs <sihorjs@gmail.com>
2019-04-03 17:26:49 +0300

    move product state to state file

--- types/state.d.ts
+++ types/state.d.ts
@@ -5,10 +5,12 @@ import reducer from "reducers/.";
 import { postAuthActions } from "actions/auth";
 import { getCarouselActions } from "actions/carousel"
 import { getCategoriesActions } from "actions/categories";
+import { getProductsActions } from "actions/products";
 import * as cart from "actions/cart";
 
 import CategoryType from "types/categories";
 import FeaturedProductType from "types/carousel";
+import ProductType from "types/products";
 
 export type FetchState = {
     readonly isError: boolean;
@@ -31,6 +33,11 @@ export type CategoriesState = {
 export type FeaturedProductsAction = ActionType<typeof getCarouselActions>;
 export type FeaturedProductsState = {
     readonly data: Array<FeaturedProductType>;
+} & FetchState;
+
+export type ProductsAction = ActionType<typeof getProductsActions>;
+export type ProductsState = {
+    readonly data: Array<ProductType>;
 } & FetchState;
 
 type State = StateType<typeof reducer>;
