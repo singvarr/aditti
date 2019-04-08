ab5c3f9e0e2645bac77abf44c4a5cf9d60d02137
sihorjs <sihorjs@gmail.com>
2019-04-08 13:28:42 +0300

    move test mock extensions to ts

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-            "<rootDir>/config/__mocks__/fileMock.js",
-        "\\.(css|less)$": "<rootDir>/config/__mocks__/styleMock.js"
+            "<rootDir>/config/__mocks__/fileMock.ts",
+        "\\.(css|less)$": "<rootDir>/config/__mocks__/styleMock.ts"
     },
     modulePaths: ["<rootDir>/frontend/", "<rootDir>/frontend/store"],
     setupFilesAfterEnv: ["<rootDir>/config/setupTests.ts"],
