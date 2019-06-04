0426284f16db4b0f7f9b733159b695ef1bc0546f
sihorjs <sihorjs@gmail.com>
2019-06-04 13:38:22 +0300

    add get handler to retrieve products from db

--- server/routes/product.ts
+++ server/routes/product.ts
@@ -5,6 +5,58 @@ import generateRandomProducts from "utils/generateRandomProducts";
 
 const router = Router();
 
+router.get(
+    "/",
+    (req, res): void => {
+        const DEFAULT_PAGE = 1;
+        const DEFAULT_OFFSET = 10;
+
+        let { page = DEFAULT_PAGE, limit = DEFAULT_OFFSET } = req.query;
+
+        if (page <= 0 || !Number.isInteger(page)) {
+            res.status(400).send("provide correct page number");
+        } else if (limit <= 0 || !Number.isInteger(limit)) {
+            res.status(400).send("provide correct limit of products");
+        }
+
+        ProductModel.estimatedDocumentCount()
+            .then(
+                (total): void => {
+                    const offset = limit * (page - 1);
+                    ProductModel.find(
+                        {},
+                        null,
+                        {
+                            limit,
+                            skip: offset
+                        },
+                        (error, products): void => {
+                            if (error) {
+                                res.status(500).send(
+                                    "Failed to load data from DB"
+                                );
+                            }
+
+                            res.json({
+                                data: products,
+                                meta: {
+                                    offset,
+                                    pages: Math.ceil(total / limit),
+                                    total
+                                }
+                            });
+                        }
+                    );
+                }
+            )
+            .catch(
+                (): void => {
+                    res.status(500).send("Cannot get data about product");
+                }
+            );
+    }
+);
+
 router.post(
     "/",
     (req, res): void => {
