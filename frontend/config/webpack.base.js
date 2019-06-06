46d48ff9777228d48b3bf7be1d5bd1341dd3bfe0
sihorjs <sihorjs@gmail.com>
2019-06-06 13:42:28 +0300

    use path aliases from own webpack config

--- frontend/config/webpack.base.js
+++ frontend/config/webpack.base.js
@@ -2,38 +2,22 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 require("dotenv").config();
-const PROJECT_ROOT = require("./root");
-
 const { NODE_ENV } = process.env;
 
+const PROJECT_ROOT = require("./root");
+
 module.exports = {
-    entry: path.resolve(PROJECT_ROOT, "frontend", "index.tsx"),
+    entry: path.resolve(PROJECT_ROOT, "index.tsx"),
     resolve: {
         extensions: [".ts", ".tsx", ".jsx", ".js", ".json"],
+        plugins: [new TsconfigPathsPlugin({ configFile: "tsconfig.json" })],
         alias: {
-            assets: path.join(PROJECT_ROOT, "frontend", "assets"),
-            vendor: path.join(PROJECT_ROOT, "frontend", "vendor"),
-            fixtures: path.join(PROJECT_ROOT, "frontend", "fixtures"),
-            store: path.join(PROJECT_ROOT, "frontend", "store"),
-            actions: path.join(PROJECT_ROOT, "frontend", "store", "actions"),
-            constants: path.join(
-                PROJECT_ROOT,
-                "frontend",
-                "store",
-                "constants"
-            ),
-            reducers: path.join(PROJECT_ROOT, "frontend", "store", "reducers"),
-            selectors: path.join(
-                PROJECT_ROOT,
-                "frontend",
-                "store",
-                "selectors"
-            ),
-            routes: path.join(PROJECT_ROOT, "frontend", "routes"),
-            components: path.join(PROJECT_ROOT, "frontend", "components"),
-            less: path.join(PROJECT_ROOT, "frontend", "less")
+            assets: path.join(PROJECT_ROOT, "assets"),
+            vendor: path.join(PROJECT_ROOT, "vendor"),
+            less: path.join(PROJECT_ROOT, "less")
         }
     },
     module: {
@@ -41,13 +25,11 @@ module.exports = {
             {
                 test: /\.(ts|tsx)$/,
                 exclude: /node_modules/,
-                include: path.resolve(PROJECT_ROOT, "frontend"),
                 use: "awesome-typescript-loader"
             },
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                include: path.resolve(PROJECT_ROOT, "frontend"),
                 use: "babel-loader"
             },
             {
@@ -65,7 +47,7 @@ module.exports = {
             "process.env.NODE_ENV": JSON.stringify(NODE_ENV)
         }),
         new HtmlWebpackPlugin({
-            template: path.resolve(PROJECT_ROOT, "frontend", "index.html")
+            template: path.resolve(PROJECT_ROOT, "index.html")
         }),
         new CleanWebpackPlugin()
     ],
