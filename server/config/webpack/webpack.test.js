ed7d92d3d374f61777aaeeeaaa82debecab5b1b5
sihor <sihorjs@gmail.com>
2019-06-08 16:22:16 +0300

    add separate configs for app start and testing

---
+++ server/config/webpack/webpack.test.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const WebpackShellPlugin = require("webpack-shell-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const baseConfig = require("./webpack.base");
+const PROJECT_ROOT = require("./root");
+
+module.exports = merge(baseConfig, {
+    mode: "development",
+    entry: path.join(PROJECT_ROOT, "test", "index.spec.ts"),
+    output: {
+        path: path.resolve(PROJECT_ROOT, "dist", "test"),
+        filename: "index.spec.js"
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new WebpackShellPlugin({
+            onBuildEnd: "mocha dist/test/**/*.spec.js"
+        })
+    ]
+});
