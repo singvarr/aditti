5b7c3c9b6aeab4231e62ac17c33d4454007d6d05
sihorjs <sihorjs@gmail.com>
2019-11-27 21:02:59 +0200

    split App component and entry point

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,44 @@
+import React, { Fragment } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Header from "components/Header";
+import Menu from "components/Menu";
+import Footer from "components/Footer";
+
+import CatalogueRoute from "routes/CatalogueRoute";
+import CartRoute from "routes/CartRoute";
+import SignUpRoute from "routes/SignUpRoute";
+import SignInRoute from "routes/SignInRoute";
+
+import store from "store/.";
+
+import "assets/fonts/index.css";
+import "assets/icons/flaticon.css";
+import "vendor/reset.css";
+import "less/index.less";
+
+import menu from "fixtures/menu";
+import footerLinks from "fixtures/footerLinks";
+
+const App = () => (
+    <Provider store={store}>
+        <BrowserRouter>
+            <Fragment>
+                <Header />
+                <Menu menu={menu} />
+                <main>
+                    <Switch>
+                        <Route component={CatalogueRoute} exact path="/" />
+                        <Route component={CartRoute} exact path="/cart" />
+                        <Route component={SignUpRoute} exact path="/signup" />
+                        <Route component={SignInRoute} exact path="/signin" />
+                    </Switch>
+                </main>
+                <Footer linksList={footerLinks} />
+            </Fragment>
+        </BrowserRouter>
+    </Provider>
+);
+
+export default App;
